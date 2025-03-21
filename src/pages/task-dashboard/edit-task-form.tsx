e489acdac9beb6a98b24e125b376ea750057ea32
Walid-Moussa <walidmoussa995@gmail.com>
2025-03-21 23:34:55 +0200

    add a generic popup component

--- src/pages/task-dashboard/edit-task-form.tsx
+++ src/pages/task-dashboard/edit-task-form.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useTaskContext } from "../../context/TaskContext";
 import { Task } from "../../models/task/task";
+import Popup from "../../shared/popup";
 
 interface AddTaskFormProps {
   isOpen: boolean;
@@ -36,46 +37,49 @@ export function EditTaskForm({ isOpen, onClose, task }: AddTaskFormProps) {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded shadow-md w-full max-w-md">
-        <h2 className="text-xl font-bold mb-4">Add New Task</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <div>
-            <label className="block mb-1 font-medium">Title</label>
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="w-full border border-gray-300 rounded px-3 py-2"
-              required
-            />
-          </div>
-          <div>
-            <label className="block mb-1 font-medium">Description</label>
-            <textarea
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="w-full border border-gray-300 rounded px-3 py-2"
-              rows={3}
-            />
-          </div>
-          <div className="flex justify-end gap-2">
-            <button
-              type="button"
-              onClick={onClose}
-              className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-              Save
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <Popup
+      onClose={onClose}
+      content={
+        <div>
+          <h2 className="text-xl font-bold mb-4">Add New Task</h2>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div>
+              <label className="block mb-1 font-medium">Title</label>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="w-full border border-gray-300 rounded px-3 py-2"
+                required
+              />
+            </div>
+            <div>
+              <label className="block mb-1 font-medium">Description</label>
+              <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="w-full border border-gray-300 rounded px-3 py-2"
+                rows={3}
+              />
+            </div>
+            <div className="flex justify-end gap-2">
+              <button
+                type="button"
+                onClick={onClose}
+                className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      }
+    ></Popup>
   );
 }
---
+++ src/shared/popup.tsx
@@ -0,0 +1,24 @@
+import closeIcon from "../assets/icons/x.svg";
+
+interface PopupProps {
+  content: React.ReactNode;
+  onClose: () => void;
+}
+
+const Popup: React.FC<PopupProps> = ({ content, onClose }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white p-6 rounded shadow-md w-full max-w-md relative">
+        <div
+          className="pd-2 absolute top-6 right-6 cursor-pointer"
+          onClick={onClose}
+        >
+          <img src={closeIcon} className="w-4 h-4" />
+        </div>
+        {content}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
